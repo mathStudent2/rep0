5a7393512fa9f61d4ad7b4afa2072fa9620c4d6c
mathStudent2 <awas91@gmx.de>
2015-09-15 04:06:45 +0200

    branching

--- test2.hs
+++ test2.hs
@@ -10,32 +10,6 @@ echo2 = do
 	v <- getLine
 	putStrLn v
 	echo2
-
-
-	
-a :: Int -> Int -> Int
-a x y = x + y
-
-(myop) = a
-
--- funktioniert nicht mit Listen, weil nur bis WHN ausgewertet wird und diese schon so vorliegen
-func :: Int -> Int
-func 0 = 0
-func n = 1 + func (n - 1)
-
-test :: IO()
-test = seq (putStrLn "hallo") (putStrLn "nr2")
-
-f (x:xs) = x
-
-main = do
-	n <- getLine
-	putStrLn "Starting..."
-	start <- getCPUTime
-	seq (func $ read n) (return ())
-	end <- getCPUTime
-	let diff = (fromIntegral (end - start)) / (10^12)
-	printf "Computation time: %0.7f sec\n" (diff :: Double)
 	
 
 
