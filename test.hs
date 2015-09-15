6f9120f2ba247ea3c084d1a437d352340cd69749
mathStudent2 <awas91@gmx.de>
2015-09-15 03:36:16 +0200

    first commit

---
+++ test.hs
@@ -0,0 +1,42 @@
+import System.CPUTime
+import Text.Printf
+
+echo :: IO ()
+echo = getLine >>= putStrLn >>= \_ -> echo
+
+-- echo in do-Notation
+echo2 :: IO ()
+echo2 = do
+	v <- getLine
+	putStrLn v
+	echo2
+
+
+	
+a :: Int -> Int -> Int
+a x y = x + y
+
+(myop) = a
+
+-- funktioniert nicht mit Listen, weil nur bis WHN ausgewertet wird und diese schon so vorliegen
+func :: Int -> Int
+func 0 = 0
+func n = 1 + func (n - 1)
+
+test :: IO()
+test = seq (putStrLn "hallo") (putStrLn "nr2")
+
+f (x:xs) = x
+
+main = do
+	n <- getLine
+	putStrLn "Starting..."
+	start <- getCPUTime
+	seq (func $ read n) (return ())
+	end <- getCPUTime
+	let diff = (fromIntegral (end - start)) / (10^12)
+	printf "Computation time: %0.7f sec\n" (diff :: Double)
+	
+
+
+
